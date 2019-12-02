5f4870108efcf2d1365035c2d34eda75bd0a0649
AaronDavid7085 <47196131+AaronDavid7085@users.noreply.github.com>
2019-12-02 18:34:56 +0000

    Revert "Merge branch 'master' of https://github.com/SWillis31/Capstone"
    This reverts commit d6521ecfaad4536d5b22075153492c2a01878f3f, reversing
    changes made to 6a8b25fa9511bea247ce6d00b30565f9621842cc.

--- capstone_test.sql
+++ capstone_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 02, 2019 at 05:39 PM
+-- Generation Time: Dec 02, 2019 at 09:58 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `about_info` (
 --
 
 INSERT INTO `about_info` (`id`, `title`, `content`) VALUES
-(1, 'About', '<h1>&nbsp;</h1>\r\n\r\n<p>The Computer Science department offers courses covering a wide range of topics in Computer Science, including programming and programming languages, data structures and algorithms, assembly language, computer architecture, networking, operating systems, compilers, software engineering, file structures, database systems, graphics, artificial intelligence, and the theory of computation. The department seeks to prepare students both for careers in the computing industry and for advanced study in Computer Science. The department maintains close ties with local computing industries and encourages its students to participate in the many cooperative and internship opportunities made available through these contacts.</p>\r\n\r\n<p>The program educational objectives are:</p>\r\n\r\n<ul>\r\n	<li>Be successful in their careers as computer scientists in business, industry, or for advanced studies in the discipline.</li>\r\n	<li>Apply fundamental principles and practices of computer science for the design, development, and management of software systems.</li>\r\n	<li>Serve as role models of ethical and responsible behavior and foster teamwork and cooperation in their profession and their communities.</li>\r\n	<li>Engage in life-long learning, embracing the latest practices, methods, and technologies in their careers.</li>\r\n</ul>\r\n\r\n<p>The program has measurable program results that enable students, by the time of graduation, to have met the following ABET-approved student outcomes (SO&rsquo;s):</p>\r\n\r\n<ul>\r\n	<li>Analyze a complex computing problem and to apply principles of computing and other relevant disciplines to identify solutions.</li>\r\n	<li>Design, implement and evaluate a computing-based solution to meet a given set of computing requirements in the context of the program&rsquo;s discipline.</li>\r\n	<li>Communicate effectively in a variety of professional contexts.</li>\r\n	<li>Recognize professional responsibilities and make informed judgments in computing practice based on legal and ethical principles.</li>\r\n	<li>Function effectively as a member or leader of a team engaged in activities appropriate to the program&rsquo;s discipline.</li>\r\n	<li>Apply computer science theory and software development fundamentals to produce computing-based solutions.</li>\r\n</ul>\r\n\r\n<p>&nbsp;</p>\r\n');
+(1, 'About', '<p>about page text</p>\r\n\r\n<p>edited</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -88,8 +88,9 @@ CREATE TABLE IF NOT EXISTS `articles` (
 --
 
 INSERT INTO `articles` (`poster`, `created`, `title`, `content`, `post_id`) VALUES
-('Sam Willis', '2019-09-18 00:00:00', 'title', '<p>Today at UA Little Rock 4 students have come to their final day where they will be representing their project. This project will determine how their end of the year will look like and would their professor be impressed or ashamed of his students. This project represents what they have learned in their years in college and how they would use their knowledge. Stay tune in the next 24 hours for the live update on their project.   </p>\r\n', 1),
-('newAdmin', '2019-12-01 00:00:00', 'About', 'Welcome to UA Little Rock! ', 13);
+('Sam Willis', '2019-09-18 00:00:00', 'title', '<p>This is an article that has a bunch of text. The quick brown fox jumps over the lazy dog.</p>\r\n', 1),
+('newAdmin', '2019-12-01 00:00:00', 'About', 'About content here', 13),
+('newAdmin', '2019-12-01 00:34:23', 'Font Test', '<h1>Test Heading</h1>\r\n\r\n<h2>Sub Heading</h2>\r\n\r\n<h3>Even Smaller Heading</h3>\r\n\r\n<p>Normal Text</p>\r\n\r\n<pre>\r\nFormatted Text</pre>\r\n', 11);
 
 -- --------------------------------------------------------
 
@@ -153,15 +154,14 @@ CREATE TABLE IF NOT EXISTS `opportunities` (
   `opp_title` varchar(255) NOT NULL,
   `opp_description` text NOT NULL,
   PRIMARY KEY (`opp_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `opportunities`
 --
 
 INSERT INTO `opportunities` (`opp_id`, `opp_title`, `opp_description`) VALUES
-(6, 'Cybersecurity', '<p>With over 300,000 open positions nation-wide in cybersecurity today and 1.1 million expected by 2021, the UA Little Rock Computer Science Department focuses on providing students the education and opportunities they need to thrive in this challenging discipline. We invite you to come to be a part of a community working hard to protect and defend our society against cybersecurity threats.&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Discipline Description</strong></p>\r\n\r\n<p>UA Little Rock a longstanding designation as a DHS and NSA Center of Academic Excellence in Information Assurance and provides the course offerings to prepare future cybersecurity professionals to hit the ground running in their careers. Our faculty come with rich industry experience and have a passion to build your skills and interest in cybersecurity defense operations. Our curriculum is dynamic to meet the constantly changing threat landscape in cybersecurity.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Job Opportunities&nbsp;</strong></p>\r\n\r\n<p>UA Little Rock sits in the capital city with two large military bases and numerous financial, energy, IT services and retail organizations. We have a strong relationship with our industry partners and work to identify local internships and job placements for our graduates. Through our research and workforce development initiatives, students are able to work closely with industry, the military, and government agencies throughout their academic careers.</p>\r\n\r\n<p>The average salaries for cybersecurity Analyst and related positions are as follows:</p>\r\n\r\n<ul>\r\n	<li>Application Security Engineer: $100,000 to $210,000</li>\r\n	<li>Network Security Analyst:$90,000 to $150,000</li>\r\n	<li>IS Security Manager:$120,00 to $180,000</li>\r\n	<li>Cybersecurity Analyst:$90,000 to $185,000</li>\r\n	<li>IS Security Engineer:$90,000 to $150,000</li>\r\n	<li>Penetration Tester:$80,000 to $130,000</li>\r\n</ul>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Projects</strong></p>\r\n\r\n<p>Here, students have the opportunity to work in cutting edge cybersecurity research both as graduates and undergraduates. Through NSF grant funding and in partnership with the Arkansas Department of Education and local industry, we develop enterprise-quality workouts in the UA Little Rock Cyber Gym. These workouts are immediately distributed to middle and high school students throughout the nation. The UA Little Rock Cyber Gym provides a safe and highly accessible cloud environment for students to learn hands-on cybersecurity skills. Our student researchers work to develop new competition-style cybersecurity workouts and find more effective techniques to distribute the content through cloud technologies.</p>\r\n\r\n<p>Housed in the Emerging Analytics Center, we also engage students in applied cybersecurity research using machine learning and 3-D visualization to target risk reduction. With over $15 million in funding, students work hand-in-hand with government and industry partners to develop solutions to the toughest challenges in cybersecurity.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>For more information, contact:</p>\r\n\r\n<p><a href=\"https://ualr.edu/computerscience/about/faculty-and-staff/philip-huff/\">Mr. Philip Huff</a><br />\r\nOffice: EIT 573<br />\r\nEmail:&nbsp;<a href=\"mailto:pdhuff@ualr.edu\">pdhuff@ualr.edu</a></p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n'),
-(9, 'Data Visualization', '<p>When a computer screen alone is not enough to represent needed, special tools such as Virtual/Augmented Reality headsets or CAVE (CAVE Automatic Virtual Environments) are needed. Data Visualization at its core is the development of applications dedicated to the representation of complex data utilizing mediums ranging from the humble LCD screen to the emerging technologies of tomorrow. Immerse yourself in the data and manipulate it at your fingertips with a career in Data Visualization.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Discipline Description</strong></p>\r\n\r\n<p>Typically when visualizing data on paper, simple 2-dimensional graphs are used. When there is data that needs more than an X and a Y-axis, animations on a computer screen are used. What happens when the data that needs to be displayed has more than 3 axis? What happens when the data that needs to be visualized has more axis than you can count on two hands? What happens when your data has hundreds of axis? Visualization has the tools and techniques to accomplish this, but you are the one to put them to use.&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Job Opportunities&nbsp;</strong></p>\r\n\r\n<p>Computer Information Research Scientists:</p>\r\n\r\n<ul>\r\n	<li>Median Salary: $118,370 per year</li>\r\n	<li>Job Growth: 16%</li>\r\n	<li>Increase in Jobs 2018-28: 5,200</li>\r\n</ul>\r\n\r\n<p>Computer Programmer:</p>\r\n\r\n<ul>\r\n	<li>Median Salary: $84,280</li>\r\n	<li>No. of Jobs 2018: 250,300</li>\r\n</ul>\r\n\r\n<p>Computer Systems Analysts:</p>\r\n\r\n<ul>\r\n	<li>Median Salary: $88,740</li>\r\n	<li>Job Growth: 9%</li>\r\n	<li>Increase in Jobs 2018-28: 56,000</li>\r\n</ul>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>Projects</strong></p>\r\n\r\n<p>The&nbsp;<a href=\"http://eac-ualr.org/\">UA Little Rock&rsquo;s EAC</a>&nbsp;in collaboration with the&nbsp;<a href=\"https://seeds.thepower.group/\">SEEDS Cybersecurity Center</a>&nbsp;is developing a virtual-reality enabled visualizations network attacks in the electric power distribution industry.&nbsp;</p>\r\n\r\n<p>The Emerging Analytics Center at UA Little Rock has also participated in a past project for the visualization of multidimensional marketing data.</p>\r\n\r\n<p>Housed in the Emerging Analytics Center, we also engage students in applied cybersecurity research using machine learning and 3-D visualization to target risk reduction. With over $15 million in funding, students work hand-in-hand with government and industry partners to develop solutions to the toughest challenges in cybersecurity.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>For more information, contact:</p>\r\n\r\n<p><a href=\"https://ualr.edu/computerscience/about/faculty-and-staff/jan-springer/\">Dr. Jan Springer</a><br />\r\nOffice: EIT 577<br />\r\nEmail:&nbsp;<a href=\"mailto:jpspringer@ualr.edu\">jpspringer@ualr.edu</a></p>\r\n\r\n<p>&nbsp;</p>\r\n');
+(6, 'Cybersecurity', '<h1>Cybersecurity</h1>\r\n\r\n<p>With over 300,000 open positions nation-wide in cybersecurity today and 1.1 million expected by 2021, the UA Little Rock Computer Science Department focuses on providing students the education and opportunities they need to thrive in this challenging discipline. We invite you to come to be a part of a community working hard to protect and defend our society against cybersecurity threats.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>Discipline Description</p>\r\n\r\n<p>Job Opportunities</p>\r\n\r\n<p>Projects</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>For more information, contact:</p>\r\n\r\n<p><a href=\"https://ualr.edu/computerscience/about/faculty-and-staff/philip-huff/\">Mr. Philip Huff</a><br />\r\nOffice: EIT 573<br />\r\nEmail:&nbsp;<a href=\"mailto:pdhuff@ualr.edu\">pdhuff@ualr.edu</a></p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>test edit</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -204,15 +204,15 @@ CREATE TABLE IF NOT EXISTS `resources` (
   `resource_name` varchar(255) NOT NULL,
   `resource_link` varchar(255) NOT NULL,
   PRIMARY KEY (`resource_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `resources`
 --
 
 INSERT INTO `resources` (`resource_id`, `resource_name`, `resource_link`) VALUES
-(7, 'UA Little Rock', 'ualr.edu'),
-(6, 'Google', 'google.com');
+(1, 'StackOverflow', 'https://www.stackoverflow.com'),
+(4, 'Student Forums', 'forum.php');
 
 -- --------------------------------------------------------
 
@@ -227,6 +227,14 @@ CREATE TABLE IF NOT EXISTS `student_orgs` (
   `org_description` text NOT NULL,
   PRIMARY KEY (`org_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_orgs`
+--
+
+INSERT INTO `student_orgs` (`org_id`, `org_name`, `org_description`) VALUES
+(1, 'Cybersecurity Club test', '<p>asdf</p>\r\n\r\n<p>test edit</p>\r\n'),
+(2, 'UALR ACM', '<p>ACM Description</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -266,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
@@ -279,14 +287,7 @@ INSERT INTO `users` (`username`, `password`, `id`, `role`) VALUES
 ('roleTest', '$2y$10$J6TZDJMMuYnHCFOcHoNMF.bSJzWJXF2yaroyHIKNh1rfoTvNPH0QK', 5, 'admin'),
 ('newUser', '$2y$10$G.04KI7AyjZynMO4ZJv5C.CjCjd2h99y.LyF/Qw47yut9NdF3hPzy', 8, 'user'),
 ('testUser', '$2y$10$XQayvvA9EUwphjsr1YVvfOt1bYflp2xQ7j9G7brtkhq4IEXV0vEiq', 9, 'user'),
-('foo', '$2y$10$NHiQEYsTNpl/Zv01hqFF1OYCwMWl1XQGEnpvC3k3qqjfuzL8Jn4ia', 21, 'admin'),
-('newAaron', '$2y$10$OZGZMJKT7Xe9dfZR1Y3bsOu4LdMSBo7/6JGFmbeyeLReo0GwzjlvO', 22, 'user'),
-('notAaron', '$2y$10$wQIDlfDL7/l.kxv.uN.ZQO/WtTYqOeqiHZnmAGntNRAtBXRzAU.YC', 23, 'user'),
-('bobdylan', '$2y$10$rs4pvlVucHocFiyemLFG2Oc7.ZP1aii8TOAcr9maotd7PSTgBICH.', 24, 'user'),
-('bobdylan2', '$2y$10$IYa/F.LSUqobA4/ZLbwy5.K/w91Sg1E274PkJ8zgcrkVZ4J4EPLKC', 25, 'user'),
-('bobbydylan', '$2y$10$xGBIZuma3bSZHu1iPYK7TuzaHKsiSY8L42cvaBf88GQcZPCGC1UHu', 26, 'user'),
-('ishaisa', '$2y$10$3HFJmEAzErSfha2nN6L4VOB5.gMNobgDJad8YpjUmf4LHT9ci7Hpu', 27, 'user'),
-('AaronDavid', '$2y$10$UZeHYla5SQWk9f.FeTcAOO3nJ11Wz6Kef6EGMXsjfRq8mqp6kgGYC', 28, 'admin');
+('foo', '$2y$10$NHiQEYsTNpl/Zv01hqFF1OYCwMWl1XQGEnpvC3k3qqjfuzL8Jn4ia', 21, 'admin');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
